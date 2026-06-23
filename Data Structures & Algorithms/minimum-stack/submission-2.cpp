fc01af2045f845ee8c6256b391855304b063a995
Pranjal Goyal <47600057+pranjalg13@users.noreply.github.com>
2026-06-23 11:21:53 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,23 @@
+class MinStack {
+public:
+    stack<pair<int,int>> st;
+    MinStack() {        
+    }
+    
+    void push(int val) {
+        int mn = st.empty() ? val : min(val, st.top().second);
+        st.push({val, mn});
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
